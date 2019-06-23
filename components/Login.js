80a7f3a3c79ed9260d55215e8c403fb52385b104
Vatsal Mistry <vatsalmistry4796@gmail.com>
2019-06-23 14:56:27 -0500

    starting form

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, TextInput, KeyboardAvoidingView} from 'react-native';
+import {Platform, StyleSheet, Text, View, SafeAreaView,StatusBar, TextInput, KeyboardAvoidingView, Dimensions} from 'react-native';
 import { connect } from 'react-redux';
 import {changeCurPage, authenticate} from '../actions/actions.js';
 import {bindActionCreators} from 'redux';
+import {Input, Button, ThemeProvider} from 'react-native-elements';
 
 class Login extends Component {
     constructor(props){
@@ -10,42 +11,73 @@ class Login extends Component {
         this.state = {
             username:"",
             password:"",
+            isFocused:false,
         }
     }
+    handleFocus = () => this.setState({ isFocused: true });
+    handleBlur = () => this.setState({ isFocused: false });
+
+    
+  
 
     render() {
+    const { label, ...props } = this.props;
+    const { isFocused } = this.state;
+
+    const theme = {
+      colors: {
+        primary: '#2c2f33',
+        color:'red'
+      
+      }
+    }
+
+    const labelStyle = {
+      position: 'absolute',
+      left: 0,
+      top: !isFocused ? 18 : 0,
+      fontSize: !isFocused ? 20 : 14,
+      color: !isFocused ? '#aaa' : '#000',
+    };
       return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <SafeAreaView style={styles.safeArea} behavior="padding" enabled>
+
           <View style={styles.container}>
-          <Text style={styles.welcome}>Login</Text>
-          <View style={{ backgroundColor: '#FDFEFE' }}>
-            <TextInput style = {styles.form} placeholder="Username" onChangeText={(username) => this.setState({username})}/>
+            <StatusBar hidden = {true}/>
+            <Text style={styles.welcome}>Login</Text>
           </View>
-          <View style={{ backgroundColor: '#FDFEFE' }}>
-            <TextInput style = {styles.form} placeholder="Password" onChangeText={(password) => this.setState({password})}/>
+          
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          
+            <TextInput style = {styles.form} underlineColorAndroid='transparent' placeholder="          Username          " onChangeText={(username) => this.setState({username})}/>
           </View>
+
+
+          <View style={{justifyContent: 'center', alignItems: 'center',marginTop: 30}}>
+            <TextInput style = {styles.form}  underlineColorAndroid='transparent'  placeholder="          Password          " onChangeText={(password) => this.setState({password})}/>
           </View>
-          <Button title = "Enter" onPress = {() => {this.props.authenticate(this.state.username, this.state.password); this.props.changeCurPage("ScoutDashboard");} }/>
-          <Button title = "Forgot Credentials" onPress = {() => {this.props.changeCurPage("ForgotPass")}}/>
-          <Button title = "Sign Up" onPress = {() => {this.props.changeCurPage("SignUp")}}/>
-        </KeyboardAvoidingView>
+
+          <ThemeProvider theme={theme} >
+            <Button style= {{backgroundColor:'orange'}} title = "Forgot Credentials?" titleStyle={{color:'#1974E5'}} onPress = {() => {this.props.changeCurPage("ForgotPass")}}/>
+          </ThemeProvider>
+          
+
+          <View style = {{width:'50%', marginTop:40, marginLeft: (Dimensions.get('screen').width/4)}}>           
+            <Button title = "Enter" onPress = {() => {this.props.authenticate(this.state.username, this.state.password); this.props.changeCurPage("ScoutDashboard");} } /> 
+          </View>
+        </SafeAreaView>
       );
     }
   }
   
   const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#23272a',
-      },
       welcome: {
         fontSize: 40,
         textAlign: 'center',
-        margin: 10,
         fontWeight: 'bold',
         color: "#99aab5",
+        marginTop:130,
+        marginBottom:60,
       },
       instructions: {
         textAlign: 'center',
@@ -53,8 +85,19 @@ class Login extends Component {
         marginBottom: 5,
       },
       form:{
-        height: 40,
-      }
+        textAlign: 'center',
+        height: 50,
+        borderWidth: 2,
+        borderColor: '#1974E5', 
+        borderRadius: 20 ,
+        backgroundColor : '#2c2f33',
+        
+        
+      },
+      safeArea: {
+        flex: 1,
+        backgroundColor: '#2c2f33'
+      },
     });
     
     function mapStateToProps(state){
--- components/Startup.js
+++ components/Startup.js
@@ -1,58 +1,160 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, SafeAreaView, TextInput, Dimensions, StatusBar} from 'react-native';
+import {changeCurPage, authenticate} from '../actions/actions.js';
+
 import { connect } from 'react-redux';
-import {changeCurPage} from '../actions/actions.js';
 import {bindActionCreators} from 'redux';
+import {ThemeProvider,Button, Divider, Input } from 'react-native-elements';
+import Login from './Login.js';
+
+
+
 
 class Startup extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+        username:"",
+        password:"",
+        isFocused:false,
+    }
+}
+
+handleFocus = () => this.setState({ isFocused: true });
+handleBlur = () => this.setState({ isFocused: false });
+
+
+
   render() {
+    const { label, ...props } = this.props;
+    const { isFocused } = this.state;
+
+    const theme = {
+      colors: {
+        primary: '#2c2f33',
+        color:'red'
+      
+      }
+    }
+
+    const labelStyle = {
+      position: 'absolute',
+      left: 0,
+      top: !isFocused ? 18 : 0,
+      fontSize: !isFocused ? 20 : 14,
+      color: !isFocused ? '#aaa' : '#000',
+    };
     return (
-        <View style={styles.container}>
-        <Text style={styles.welcome}>Steel Scout</Text>
-        <Button style={styles.signUpButton} title = "Sign Up" onPress = {() => {this.props.changeCurPage("SignUp")}}/>
-        <Button style={styles.logInButton} title = "Log In" onPress = {() => {this.props.changeCurPage("Login")}}/>
-      </View>
+
+      <SafeAreaView style={styles.safeArea}>
+
+        <View style = {styles.container}>
+            <StatusBar hidden = {true}/>
+            <Text style={[styles.welcome]}>Steel Scout</Text>
+        </View>
+
+        <View style = {styles.container2}>
+          <Image source={require('./images/SteelPic.jpg')} style ={styles.icon}/>
+        </View>
+
+        
+      <View style = {styles.container2}>
+          <Text style={[styles.instructions]}>Official 5427 Steel Talons Scouting App!</Text>
+        </View>
+        
+        <View style = {styles.container2}>
+          <Text style={{fontSize:15}}> ────────  Welcome  ──────── </Text>
+        </View>
+
+        <View style={{justifyContent: 'center', alignItems: 'center', marginTop:40}}>      
+            <TextInput style = {styles.form} underlineColorAndroid='transparent' placeholder="          Username          " onChangeText={(username) => this.setState({username})}/>
+        </View>
+        <View style={{justifyContent: 'center', alignItems: 'center',marginTop: 30}}>
+          <TextInput style = {styles.form}  underlineColorAndroid='transparent'  placeholder="          Password          " onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+      
+        <View style = {{width:'50%', marginTop:40, marginLeft: (Dimensions.get('screen').width/4)}}>           
+            <Button title = "Log In" onPress = {() => {this.props.authenticate(this.state.username, this.state.password); this.props.changeCurPage("ScoutDashboard");} } /> 
+        </View>
+        
+     
+      
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#23272a',
-    },
-    welcome: {
-      fontSize: 40,
-      textAlign: 'center',
-      margin: 10,
-      fontWeight: 'bold',
-      color: "#99aab5",
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#99aab5',
-      marginBottom: 5,
-    },
-    signUpButton:{
-      margin: 10,
-    },
-    logInButton:{
-      margin: 10,
-    }
+  container: {
+    flex:0,
+    width: Dimensions.get('screen').width, 
+    backgroundColor: '#1974E5',
+    alignItems : 'center',
+    borderBottomWidth : 1,
+    borderBottomColor : 'gray',
+  },
+  container2: {
+    flex:0,
+    marginTop : 60,
+    width: Dimensions.get('screen').width, 
+    backgroundColor: '#2c2f33',
+    alignItems : 'center',
+  },
+  icon:
+  {
+    width: 100,  
+    height: 100,
+    borderRadius : 150/2,
+    backgroundColor: 'black',
+  },
+  welcome: {
+
+    fontSize: 50,
+    color: 'white',
+    
+    textAlign: 'center',
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#2c2f33'
+  },
+  instructions: {
+    fontSize: 20,
+    color: 'black',
+    
+    textAlign: 'center',
+    
+  },
+  form:{
+    textAlign: 'center',
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#1974E5', 
+    borderRadius: 20 ,
+    backgroundColor : '#2c2f33',
+    
+    
+  },
   });
   
   function mapStateToProps(state){
     return{
         // username: state.auth.username,
         // password: state.auth.password,
+        auth: state.auth,
         curPage: state.curPage,
     }
   }
 
-function matchDispatchToProps(dispatch){
-    return bindActionCreators({changeCurPage: changeCurPage}, dispatch);
+  function matchDispatchToProps(dispatch){
+    return bindActionCreators(
+      {
+        changeCurPage: changeCurPage,
+        authenticate: authenticate,
+      }
+      , dispatch);
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(Startup);
